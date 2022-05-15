bd282384d26b894ed55b587b74535ce404ce8457
abdullahalshawafi <abdullahadel.aam@gmail.com>
2022-05-15 22:14:20 +0200

    Updated About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,13 @@
 <template>
-    <h1>About</h1>
-    <h3>Version 1.0.0</h3>
-    <p>This is a simple todo app built with Vue.js and Vue Router.</p>
-    <router-link to="/">Go back</router-link>
+  <h1>About</h1>
+  <h3>Version 1.0.0</h3>
+  <p>This is a simple todo app built with Vue.js and Vue Router.</p>
+  <router-link to="/">Go back</router-link>
 </template>
+
+<script>
+export default {
+  name: "About",
+  inheritAttrs: false,
+};
+</script>
